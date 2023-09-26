3b4c3d62f5e3f3d48fd80b31a5a1bb5651653b8e
Mark Tanser <tanser.mark@gmail.com>
2023-09-26 10:37:15 +0900

    initialise for github repo

---
+++ android/app/src/main/kotlin/com/example/firebase_auth_logins/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.firebase_auth_logins
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/constants/constants.dart
@@ -0,0 +1,4 @@
+class Constants {
+  // TODO: Replace this with your firebase project URL
+  static const String firebaseProjectURL = 'https://fir-authlogins-8345e.firebaseapp.com';
+}
